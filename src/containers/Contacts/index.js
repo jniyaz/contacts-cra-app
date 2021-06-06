8b9b527703fce4db0d7ecb2444012afbc260c37a
Niyaz <niyaz.jamal@icarasia.com>
2021-06-06 17:44:33 +0800

    App setup and routes integration

---
+++ src/containers/Contacts/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactsContainer = () => {
+    return (
+        <div>
+            <h2>Contacts</h2>
+        </div>
+    )
+}
+
+export default ContactsContainer
---
+++ src/containers/CreateContact/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateContactContainer = () => {
+    return (
+        <div>
+            <h2>Create Contact</h2>
+        </div>
+    )
+}
+
+export default CreateContactContainer
